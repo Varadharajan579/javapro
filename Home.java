89586a52efa9e1f34ca78ca6d976d2dbcdf3d0c7
Your Name <you@example.com>
2025-05-18 15:34:00 +0530

    updated

--- Home.java
+++ Home.java
@@ -1,8 +1,9 @@
 public class Home{
     public static void main(String args[]){
-        System.out.println("possive numbers from 10 to 50;");
+      //  System.out.println("positive numbers from 10 to 50;");
          for(int i=10;i<=50;i++){
-            System.out.println(i+"");
+           // System.out.println(i+"");
+            System.out.println("The Positive  number from 10 to 50 is: "+i);
          }
     }
 }
